335f30a687053b38cb3b4db631d4c137de465e1b
Nitesh Gautam <138124708+niteshgautam12@users.noreply.github.com>
2025-09-01 15:46:45 +0530

    Add files via upload

---
+++ i am rich/i am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am rich
+//
+//  Created by Nitesh Gautam on 01/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ i am rich/i am richTests/i_am_richTests.swift
@@ -0,0 +1,17 @@
+//
+//  i_am_richTests.swift
+//  i am richTests
+//
+//  Created by Nitesh Gautam on 01/09/25.
+//
+
+import Testing
+@testable import i_am_rich
+
+struct i_am_richTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
